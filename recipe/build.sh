0fc3f48a7249d3aadfe239c636dbe4f9c9ac107c
Daniel Nachun <daniel.nachun@gmail.com>
2025-10-09 19:45:15 -0700

    fix TBB on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Patch configure script to use LDFLAGS so that proper RPATH is set for libtbb test binary
+sed -i 's/${CXX} ${CXXFLAGS} ${TBBFLAGS}/${CXX} ${CXXFLAGS} ${LDFLAGS} ${TBBFLAGS}/' configure
+
 export DISABLE_AUTOBREW=1
 $R CMD INSTALL --build . ${R_ARGS}
