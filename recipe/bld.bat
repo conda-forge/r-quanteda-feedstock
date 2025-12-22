a3fe324180fd8b194bcb895fa575eaec221684c3
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-22 17:20:48 -0600

    try r_clang

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,11 @@
-copy "%RECIPE_DIR%\Makevars.ucrt" "src\Makevars.ucrt"
-IF %ERRORLEVEL% NEQ 0 exit 1
+:: copy "%RECIPE_DIR%\Makevars.ucrt" "src\Makevars.ucrt"
+:: IF %ERRORLEVEL% NEQ 0 exit 1
 
-set PATH=%PATH%;%PREFIX%\lib\R\library\RcppParallel\lib\x64
+:: set PATH=%PATH%;%PREFIX%\lib\R\library\RcppParallel\lib\x64
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit 1
 
 :: Copy tbb.dll from RcppParallel folder to same folder as quanteda.dll
-copy %PREFIX%\lib\R\library\RcppParallel\lib\x64\tbb.dll %PREFIX%\lib\R\library\quanteda\libs\x64
-IF %ERRORLEVEL% NEQ 0 exit 1
+:: copy %PREFIX%\lib\R\library\RcppParallel\lib\x64\tbb.dll %PREFIX%\lib\R\library\quanteda\libs\x64
+:: IF %ERRORLEVEL% NEQ 0 exit 1
