6105100b4f2e064ba96482f8344987f8db3ff52a
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-22 14:56:00 -0600

    extra fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,5 +7,5 @@ set PATH=%PATH%;%PREFIX%\lib\R\library\RcppParallel\lib\x64
 IF %ERRORLEVEL% NEQ 0 exit 1
 
 :: Copy tbb.dll from RcppParallel folder to same folder as quanteda.dll
-:: copy %PREFIX%\lib\R\library\RcppParallel\lib\x64\tbb.dll %PREFIX%\lib\R\library\quanteda\libs\x64
-:: %ERRORLEVEL% NEQ 0 exit 1
+copy %PREFIX%\lib\R\library\RcppParallel\lib\x64\tbb.dll %PREFIX%\lib\R\library\quanteda\libs\x64
+IF %ERRORLEVEL% NEQ 0 exit 1
