76f078383ea0fcd806c2e1298b5bb2e5c8bc0693
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-22 18:57:53 -0600

    cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,9 @@
-:: copy "%RECIPE_DIR%\Makevars.ucrt" "src\Makevars.ucrt"
-:: IF %ERRORLEVEL% NEQ 0 exit 1
-
-:: set PATH=%PATH%;%PREFIX%\lib\R\library\RcppParallel\lib\x64
-
+:: Export symbols in RcppExports.cpp
 sed -i -e "s/void R_init_quanteda/__declspec(dllexport) void R_init_quanteda/" src/RcppExports.cpp
 IF %ERRORLEVEL% NEQ 0 exit 1
+:: Remove part of PKG_LIBS that tries to link non-existent libraries.
 sed -i -e "s/$(LAPACK_LIBS) $(BLAS_LIBS)//" src/Makevars.win
 IF %ERRORLEVEL% NEQ 0 exit 1
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit 1
-
-:: Copy tbb.dll from RcppParallel folder to same folder as quanteda.dll
-:: copy %PREFIX%\lib\R\library\RcppParallel\lib\x64\tbb.dll %PREFIX%\lib\R\library\quanteda\libs\x64
-:: IF %ERRORLEVEL% NEQ 0 exit 1
